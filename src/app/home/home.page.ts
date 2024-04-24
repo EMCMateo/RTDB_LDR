3b46739767f09fb0d142123b2b4226ff649d00ee
EMCMateo <mateo5678moli@gmail.com>
2024-04-24 10:49:42 -0500

    ins

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,41 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Database, object, ref } from '@angular/fire/database';
-
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   ldrValue: number = 0;
-  constructor(private database:Database) {
-  }
+  luz: number = 0;
+
+  constructor(private database: Database) {}
 
   ngOnInit() {
-    const route = ref(this.database, 'ldrValue'); // Ruta en la base de datos donde se almacena el valor del sensor LDR
+    const route = ref(this.database, 'ldrValue');
     object(route).subscribe(attributes => {
-      this.ldrValue = attributes.snapshot.val(); // Asigna el valor obtenido del sensor LDR a la variable ldrValue
-      console.log(this.ldrValue);
+      this.ldrValue = attributes.snapshot.val(); 
+      console.log(this.ldrValue);      
+      this.luz = this.ldrValue;
+
+      if (this.luz > 890) {
+        this.scheduleNotification("Dia", "El valor del sensor LDR es mayor a 890.");
+      } else {
+        this.scheduleNotification("Noche", "El valor del sensor LDR es menor a 890.");
+      }
+    });
+  }
+
+  async scheduleNotification(title: string, body: string) {
+    const notification = {
+      title: title,
+      body: body,
+      id: 1 
+    };
+    await LocalNotifications.schedule({
+      notifications: [notification]
     });
   }
 }
